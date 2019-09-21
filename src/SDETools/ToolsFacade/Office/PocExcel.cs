877d1ba66134c28091c0596aadaf0f0c81a92677
Christian Popescu <christian.popescu@outlook.com>
2019-09-22 00:14:16 +0200

    Add Excel Facade Poc

---
+++ src/SDETools/ToolsFacade/Office/PocExcel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ToolsFacade.ExcelFacade
+{
+    class PocExcel
+    {
+        static void PocOpenDelimitedInExcel()
+        {
+            Excel.Application xlApp;
+            //Excel.Workbook xlWorkBook;
+            //Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+            xlApp = new Excel.Application();
+            Excel.Workbooks xlWorkBooks = xlApp.Workbooks;
+            xlWorkBooks.OpenText(@"E:\ccp_vhdd\SI_20170426.txt",null,Excel.XlTextParsingType.xlDelimited);
+
+            //xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            //releaseObject(xlWorkSheet);
+            //releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+
+
+        }
+
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                //MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+
+    }
+}
