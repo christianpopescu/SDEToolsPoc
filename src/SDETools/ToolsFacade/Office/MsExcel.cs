b6a0092e1f27dd3b3c46301a27297e9417e37ab6
Christian Popescu <christian.popescu@outlook.com>
2019-09-22 00:38:47 +0200

    Add Microsoft Excel Class

---
+++ src/SDETools/ToolsFacade/Office/MsExcel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsFacade.Office
+{
+    public class MsExcel :OfficeInterop
+    {
+        private MsExcel _instance;
+
+        private MsExcel(){}
+
+        public MsExcel GetInstance()
+        {
+            return _instance ?? (_instance = new MsExcel());
+        }
+    }
+}
