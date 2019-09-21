8502857e8b5041a46bcecad93fb4a98950ca4abb
Christian Popescu <christian.popescu@outlook.com>
2019-09-22 00:26:29 +0200

    Add Interop object as base class for Excel, Word, Outlook \n and others

---
+++ src/SDETools/ToolsFacade/Office/OfficeInterop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsFacade.Office
+{
+    public class OfficeInterop
+    {
+        public static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+    }
+}
