be05dacd44e985ebd492cd5c7f714279f254ad4d
Christian Popescu <christian.popescu@outlook.com>
2019-09-24 23:51:55 +0200

    MsExcel and Office Interop with bind to running process

--- src/SDETools/ToolsFacade/Office/MsExcel.cs
+++ src/SDETools/ToolsFacade/Office/MsExcel.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ToolsFacade.Office
 {
-    public class MsExcel :OfficeInterop
+    public class MsExcel : OfficeInterop
     {
         private MsExcel _instance;
+        
+        private Excel.Application _excelApplication;
 
-        private MsExcel(){}
+        static MsExcel()
+        {
+            AppType = typeof(Excel.Application);
+            TypeName = "Excel.Application";
+            ProcessName = "EXCEL";
+        }
+
+        private MsExcel()
+        {
+            
+        }
 
         public MsExcel GetInstance()
         {
--- src/SDETools/ToolsFacade/Office/OfficeInterop.cs
+++ src/SDETools/ToolsFacade/Office/OfficeInterop.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ToolsFacade.Office
 {
-    public class OfficeInterop
+    public abstract class OfficeInterop
     {
+        protected Type AppType;
+        protected string TypeName;
+        protected string ProcessName;
+
+        public static T BindToRunningProcessOrNew<T>() where T : OfficeInterop
+        {
+            T t;
+            // Get the current process.
+            Process currentProcess = Process.GetCurrentProcess();
+ 
+            // Get all processes running on the local computer.
+            Process[] localAll = Process.GetProcessesByName("EXCEL");
+            Excel.Application application;
+            if (localAll.Length > 0) application = Marshal.GetActiveObject("Excel.Application") as Excel.Application;
+            else application = new Excel.Application();
+
+            return
+        }
+
         public static void ReleaseObject(object obj)
         {
             try
