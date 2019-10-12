8e04dd4038f9a3a43c26de4e3c89f8f66c02eba4
Christian Popescu <christian.popescu@outlook.com>
2019-10-12 23:53:50 +0200

    Diff classes

---
+++ src/SDETools/SDEConsole/DiffResultElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDEConsole
+{
+    public class DiffResultElement<T>
+    { 
+        T ElementFirst;
+        T ElementSecond;
+    }
+}
