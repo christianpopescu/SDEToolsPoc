382dc9ba5a1d6fc60177d8132585d9d8fca706e7
Christian Popescu <christian.popescu@outlook.com>
2019-10-13 21:46:59 +0200

    Improve diff tool compare for equal

--- src/SDETools/DiffTools/Tools/DiffTool.cs
+++ src/SDETools/DiffTools/Tools/DiffTool.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DiffTools.ResultDao;
 
-namespace DiffTools
+namespace DiffTools.Tools
 {
     public class DiffTool<T> where T :IEquatable<T>
     {
-        public static readonly DiffElement<T> NullElement = new DiffElement<T>() {Index = 0, Value = default(T)};
+        public static readonly DiffElement<T> NullElement = new DiffElement<T>(0, default(T));
         public static IList<DiffResultElement<T>> CompareForEquality(IList<T> pFirstToCompare,
             IList<T> pSecondToCompare)
         {
+            long min = Math.Min(pFirstToCompare.Count(), pSecondToCompare.Count());
+            IList<T> maxList = (pSecondToCompare.Count() == min) ? pFirstToCompare : pSecondToCompare ;
             List<DiffResultElement<T>> diffResults = new List<DiffResultElement<T>>();
-
+            for (int i = 0; i < min; i++)
+            {
+                if (!pFirstToCompare.Equals(pSecondToCompare))
+                {
+                    diffResults.Add(new DiffResultElement<T>(new DiffElement<T>(i, pFirstToCompare[i]),
+                        new DiffElement<T>(i, pSecondToCompare[i])));
+                }
+            }
 
             return diffResults;
         }
