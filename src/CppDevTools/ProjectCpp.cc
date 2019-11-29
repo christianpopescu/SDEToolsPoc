1af9317b331661ad27f162b37235dcb15494ccdf
Christian Popescu <christian.popescu@outlook.com>
2019-11-30 00:39:27 +0100

    add output file

--- src/CppDevTools/ProjectCpp.cc
+++ src/CppDevTools/ProjectCpp.cc
@@ -1,1 +1,29 @@
-#include <bits/stdc++.h>using namespace std;int main() {	cout << "End of programm " << endl;}
+#include <bits/stdc++.h>
+
+
+enum class CppVersion {
+	cpp11 = 11,
+	cpp14 = 14,
+	cpp17 = 17
+};
+
+class CppProject {
+	public:
+	CppProject() = delete;
+	CppProject(std::string name, CppVersion version) 
+	: project_name {"name"},
+	 cppVersion{CppVersion::cpp11}	{}
+	
+	private:
+	std::string project_name;
+	CppVersion cppVersion;
+};
+
+using namespace std;
+
+int main() {
+	CppProject cppp ("testproject", CppVersion::cpp14);
+	freopen("makefile.testproject","w", stdout);
+	
+	cout << "End of programm " << endl;
+}
