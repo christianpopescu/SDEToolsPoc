0225914a9a94b9035d108a1e5f5ca6cf1556aff1
Christian Popescu <christian.popescu@outlook.com>
2019-12-04 23:49:23 +0100

    new simple project generator

---
+++ src/CppDevTools/ProjectCpp/ClassElement.cc
@@ -0,0 +1,15 @@
+#include "ClassElement.h"
+
+ClassElement::ClassElement(std::string p_name): name(p_name){
+  
+}
+
+std::string ClassElement::getHeaderFileName() {
+  return name + ".h"; 
+}
+std::string ClassElement::getCppFileName() {
+  return name + ".cc"; 
+}
+std::string ClassElement::createHeaderFile() const;
+std::string ClassElement::createCodeFile() const;
+
---
+++ src/CppDevTools/ProjectCpp/ClassElement.h
@@ -0,0 +1,12 @@
+#include <string>
+class ClassElement {
+ public:
+  ClassElement() = delete;
+  ClassElement(std::string p_name);
+  std::string getHeaderFileName() const;
+  std::string getCppFileName() const;
+  std::string createHeaderFile() const;
+  std::string createCodeFile() const;
+ private:
+  std::string name;
+}
