2e3de0eb4fb95ecd8a5a5d002691e51de6470eb2
Christian Popescu <christian.popescu@outlook.com>
2020-03-01 22:08:42 +0100

    Add MsExcel class

---
+++ src/Poc/PocExcel/PocExcel/ExcelTools/MsExcel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PocExcel.ExcelTools
+{
+
+    public class MsExcel : OfficeInterops
+    {
+        private static MsExcel _instance;
+
+        private static Excel.Application _excelApplication;
+
+        private static readonly string _typeName = "Excel.Application";
+        private static readonly string _processName = "EXCEL";
+        private MsExcel()
+        {
+        }
+
+        public static MsExcel GetInstance()
+        {
+            if (_instance != null) return _instance;
+            _instance = new MsExcel();
+            try
+            {
+                _excelApplication = BindToRunningProcessOrNew();
+            }
+            catch (Exception ex)
+            {
+                // todo log exception
+                _instance = null;
+            }
+            return _instance;
+        }
+
+        protected static Excel.Application BindToRunningProcessOrNew()
+        {
+            // Get the current process.
+            Process currentProcess = Process.GetCurrentProcess();
+
+            // Get all processes running on the local computer.
+            Process[] localAll = Process.GetProcessesByName(_processName);
+            Excel.Application application;
+            if (localAll.Length > 0) application = Marshal.GetActiveObject(_typeName) as Excel.Application;
+            else application = new Excel.Application();
+
+            return application;
+        }
+    }
+}
---
+++ src/Poc/PocExcel/PocExcel/ExcelTools/OfficeInterops.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocExcel.ExcelTools
+{
+    public class OfficeInterops
+    {
+        protected static void ReleaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                // todo : log exception
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
