dae241554b2fab591f1192927bd4878ea542cdb5
Christian Popescu <christian.popescu@outlook.com>
2020-03-09 23:06:39 +0100

    Add SSH simple command

--- src/Poc/PocSshClient/PocSshClient/Program.cs
+++ src/Poc/PocSshClient/PocSshClient/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+using Renci.SshNet.Sftp;
 
 namespace PocSshClient
 {
@@ -10,6 +13,28 @@ namespace PocSshClient
     {
         static void Main(string[] args)
         {
+            TestSimpleCommand();
+
+            Console.ReadKey();
         }
+
+        static void TestSimpleCommand()
+        {
+            SshClient sshclient = new SshClient("server", "user", "password");
+            sshclient.Connect();
+            SshCommand sc = sshclient.CreateCommand("cd ..");
+            ///data/current/config
+            sc.Execute();
+            //sc = sshclient.CreateCommand("ls -al");
+            sc = sshclient.CreateCommand("ls -al /data/current/config");
+            sc.Execute();
+            string answer = sc.Result;
+            Console.WriteLine(answer);
+            sc = sshclient.CreateCommand("cat /data/apex/current/config/Environment.cfg");
+            sc.Execute();
+            answer = sc.Result;
+            Console.WriteLine(answer);
+        }
+
     }
 }
