841b3723ea39eba440b2b45a323d125121a831ec
Christian Popescu <christian.popescu@outlook.com>
2020-10-27 22:32:52 +0100

    Add File processing helper library

---
+++ src/NetCore/SDETools/FileHelper/LineChanger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FileHelper
+{
+    /// <summary>
+    /// Responsibilty : Identify line to change + transform line
+    /// </summary>
+    public abstract class LineChanger
+    {
+        protected String IdentificationPattern;
+        protected Regex regIdent;
+
+        LineChanger(String pIdentificationPattern) 
+            => (IdentificationPattern, regIdent) = (pIdentificationPattern, new Regex(IdentificationPattern));
+
+        public bool isLineToProcess(string pLine) => regIdent.IsMatch(pLine);
+
+        public abstract string Process(string pLine);
+
+    }
+}
---
+++ src/NetCore/SDETools/FileHelper/TextFileChanger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileHelper
+{
+    /// <summary>
+    /// Responsibility : Allow small changes (by line) in text file
+    /// Scenarios : Changing Configuration files, Project Files, ....
+    /// </summary>
+    public class TextFileChanger
+    {
+        public String InputFile { get; set; }
+        public String TmpFile { get; set; }
+        public String BackupFilePrefix { get; set; }
+        public String  BackupFile => BackupFilePrefix + DateTime.Now.ToString(@"yyyyMMdd_HHmmss");
+        public List<string> FileContent = new List<string>();
+    }
+}
