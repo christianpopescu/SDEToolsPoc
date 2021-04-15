723f4d0ee01a59a9b91f43639c7ecbc980a8282d
Christian Popescu <christian.popescu@outlook.com>
2021-04-16 00:01:10 +0200

    Add new project Operating system facade

---
+++ src/NetCore/SDETools/src/OSFacade/FileFolder/Interfaces/IOSDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fr.vadc.OSFacade.Interfaces
+{
+    /// <summary>
+    /// Interface to operating system Directory Class
+    /// The purpose is to facilitate the separation of client from operating system (Facilitate tests)
+    /// </summary>
+    public interface IOSDirectory
+    {
+        static IEnumerable<string> EnumerateDirectories(string pPath) => throw new NotImplementedException();
+
+        static IEnumerable<string> EnumerateFiles(string pPath) => throw new NotImplementedException();
+    }
+}
---
+++ src/NetCore/SDETools/src/OSFacade/FileFolder/WindowsImplementation/WinDirectory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fr.vadc.OSFacade.Interfaces;
+
+namespace fr.vadc.OSFacade.WindowsImplementation
+{
+    public class WinDirectory : IOSDirectory
+    {
+        public static IEnumerable<string> EnumerateDirectories(string pPath)
+        {
+            return Directory.EnumerateDirectories(pPath);
+        }
+
+        public static IEnumerable<string> EnumerateFiles(string pPath)
+        {
+            return Directory.EnumerateDirectories(pPath);
+        }
+    }
+}
