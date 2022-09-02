6d9c00abdd77a1bd7b6ff750854392731e5cac0e
Christian Popescu <christian.popescu@outlook.com>
2022-09-02 23:54:55 +0200

    Add poc archiver

---
+++ src/Poc/PocArchiver/NetFramework/PocArchiver/PocArchiver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocArchiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathSource = @"D:\ccp_vhdd_main\Portfolios\CareerManagement";
+            string pathDest = @"D:\temp\CareerManagement.zip";
+            ZipFile.CreateFromDirectory(pathSource, pathDest);
+        }
+    }
+}
